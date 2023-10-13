c3615ea0a5d96a768a2b09b2d2e7fd9f31b8d826
Martin_Gonzalez <martinjaviergonzalez_2004@hotmail.com>
2023-10-13 10:20:56 -0300

    primer commit

---
+++ index1.php
@@ -0,0 +1,6 @@
+<?php
+$cadena1="Hola";
+$cadena2="Mundo";
+echo $cadena1." ".$cadena2. "<br>" . "¡Ojo!";
+echo"<br>$cadena1 $cadena2 ". "<br>";
+?>
---
+++ index2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taller 01-Ejer03</title>
+</head>
+<body>
+    <h2>Ejercicio de Fecha</h2><br><br>
+    <?php
+    date_default_timezone_set(('America/Asuncion'));
+        $dia_ingles = date('D');
+        if ($dia_ingles == 'Sun') {
+            $dia_es = 'Domingo';
+        }elseif ($dia_ingles== 'Mon') {
+            $dia_es = 'Lunes';
+        }elseif ($dia_ingles=='Tue') {
+            $dia_es = 'Martes';
+        }elseif ($dia_ingles=='Wed') {
+            $dia_es = 'Miércoles';
+        }elseif ($dia_ingles=='Thu') {
+            $dia_es = 'Jueves';
+        }elseif ($dia_ingles=='Fri') {
+            $dia_es = 'Viernes';
+        }elseif ($dia_ingles=='Sat') {
+            $dia_es= 'Sábado';
+        }
+        echo"El día de la semana es $dia_es";
+    ?>
+    
+</body>
+</html>
